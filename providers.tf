29f6ebbb857017e11c503fc24cd501e1c28f4f8d
Bindu priya <pehlaj0405@outlook.com>
2025-10-25 20:12:19 +0530

    new changes

--- providers.tf
+++ providers.tf
@@ -6,12 +6,7 @@ terraform {
     }
   }
 
-  backend "azurerm" {
-    resource_group_name  = "myproject"
-    storage_account_name = "tfbg"
-    container_name       = "tfcontainer"
-    key                  = "newstorage-account.tfstate" # for testing2
-  }
+  
 }
 
 provider "azurerm" {
